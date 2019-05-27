b080cb3cf441e48998f1437976ffc56df8cb2ed2
IamGroooooot <dury.ko@gmail.com>
2019-05-27 21:49:06 +0900

    .

---
+++ CIEN/CIEN/Charater.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace CIEN
+{
+    public struct Vector3
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Vector3(float _x,float _y,float _z)
+        {
+            x = _x;
+            y = _y;
+            z = _z;
+        }
+
+        public void PrintElements()
+        {
+            Console.WriteLine("x: "+x+", y: "+y+", z: " + z);
+            Console.WriteLine($"x: {x}, y: {y}, z: {z}");
+        }
+
+       
+    }
+
+    public struct Image
+    {
+        string path;
+        
+        public Image(string _path)
+        {
+            path = _path;
+        }
+    }
+
+    public class Charater
+    {
+        //position scale rotation
+        public Vector3 position;
+        public Vector3 scale;
+        public Vector3 rotation;
+
+        public int health;
+        public int mana;
+        public int damage;
+        public bool isDead;
+        public string name;
+        public Image myCharactorImage;
+
+        public void SetPosition(Vector3 targetPos)
+        {
+            position = targetPos;
+        }
+        
+        public void Move(Vector3 direction)
+        {
+            SetPosition(new Vector3(position.x + direction.x, position.y + direction.y, position.z + direction.z));
+        }
+
+        public virtual bool Skill(string SkillType)
+        {
+            if (SkillType == "throw")
+            {
+                Console.WriteLine("Skill Throw");
+                return true;
+            }
+            else if (SkillType == "shield")
+            {
+                Console.WriteLine("Skill Throw");
+                return true;
+            }
+            else if (SkillType == "fireball")
+            {
+                Console.WriteLine("Skill fireball");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Error, unrecognized skillType");
+                return false;
+            }
+        }
+
+        public void Die()
+        {
+            isDead = true;
+
+            //그 외의 필요한 작업들
+            health = 100;
+            mana = 100;
+        }
+
+        public Charater(Vector3 _pos, Vector3 _rot, Vector3 _scale, int _hp, int _mana,int _damage,bool isDead,string _name,Image _img)
+        {
+            position = _pos;
+            rotation = _rot;
+            scale = _scale;
+            health = _hp;
+            mana = _mana;
+            damage = _damage;
+            this.isDead = isDead;
+            name = _name;
+            myCharactorImage = _img;
+        }
+    }
+    
+}
---
+++ CIEN/CIEN/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIEN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //save파일 불러오기
+            bool saveFileExists = true;
+            string data= "0,0,0,1,2,2,1,1,1,100,100,13,false";
+
+            if (saveFileExists)
+            {
+                var splittedData = data.Split(',');
+                
+                Vector3 savedPos = new Vector3(Convert.ToSingle(splittedData[0]), Convert.ToSingle(splittedData[1]), Convert.ToSingle(splittedData[2]));
+                Charater groot = new Charater(savedPos, new Vector3(0, 0, 0), new Vector3(1, 1, 1), 100, 100, 10, false, "그루트", new Image("/data/char.jpg"));
+            }
+            else
+            {
+                Charater groot = new Charater(new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(1, 1, 1), 100, 100, 10, false, "그루트", new Image("/data/char.jpg"));
+            }
+
+
+        }
+    }
+
+
+    
+}
